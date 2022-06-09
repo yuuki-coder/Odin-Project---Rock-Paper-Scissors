1444662b70c4106afb1f3494e851843becb7f105
yuuki-coder <eidiyuki21@gmail.com>
2022-06-09 20:15:07 -0300

    Aded prompt command

--- script.js
+++ script.js
@@ -1,104 +1,106 @@
-
-   
-   function computerPlay(){
-      let randomNumber = Math.floor(Math.random()*3)
-      if (randomNumber==0) {
-        let result = "rock"
-        return result
-      }
-      else if (randomNumber==1)
-      {
-        let result = "paper"
-        return result
-      }
-      else if (randomNumber==2)
-      {
-        let result = "scissors"
-        return result
-      }
-    }
-
-    function playRound(playerSelection, computerSelection) {
-        let scorePlayer=0
-        let scoreComputer=0
-     switch (computerSelection) {
-      
-  case 'rock':
-   if(playerSelection=='rock'){
-     return ("Draw")
-   }
-   else if(playerSelection=='paper'){
-       ++scorePlayer
-     return ("You Win")
-   }
-   else if(playerSelection=='scissors'){
-       scoreComputer++
-     return ("You Lose")
-   }
-    break;
-
-  case 'paper':
-  if(playerSelection=='rock'){
-    scoreComputer++
-     return ("You Lose")
-   }
-   else if(playerSelection=='paper'){
-     return ("Draw")
-   }
-   else if(playerSelection=='scissors'){
-    scorePlayer++
-     return ("You Win")
-   }
-    break;
-
-  case 'scissors':
-  if(playerSelection=='rock'){
-    scorePlayer++
-     return ("You Win")
-   }
-   else if(playerSelection=='paper'){
-    scoreComputer++
-     return ("You Lose")
-   }
-   else if(playerSelection=='scissors'){
-     return ("Draw")
-   }
-
-    break;
-  default:
-    console.log("Unexpected error");
+function computerPlay(){
+  let randomNumber = Math.floor(Math.random()*3);
+  if (randomNumber==0) {
+    let result = "rock";
+    return result;
+  }
+  else if (randomNumber==1)
+  {
+    let result = "paper";
+    return result;
+  }
+  else if (randomNumber==2)
+  {
+    let result = "scissors";
+    return result;
+  }
 }
-    }
 
-function game() {
-    let scorePlayer=0;
-    let scoreComputer=0;
-   while (scoreComputer < 5 && scorePlayer < 5) {
-
-        let playerSelection = "rock";
-        let computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection))
-        let result = playRound(playerSelection, computerSelection)
-        if (result == "You Win"){
-            scorePlayer ++
-        }
-        else if 
-            (result =="You Lose"){
-                scoreComputer++
-            }
-            console.log("Player Score: " + scorePlayer)
-            console.log("Computer Score: " + scoreComputer)
-            console.log ("======================================================================")
-
-        }
-        if(scoreComputer==5) {
-            console.log ("Game over, You lose")
-        }
-        else {
-            console.log("Congratulations, You are the winner!")
-        }
+function playRound(playerSelection, computerSelection) {
+  switch (playerSelection) {
     
+      case 'rock':
+       if(computerSelection=="rock"){
+         return ("Draw");
+       }
+       else if(computerSelection=="paper"){
+         return ("You Lose");
+       }
+       else if(computerSelection=="scissors"){
+         return ("You Win");
+       }
+        break;
+    
+      case 'paper':
+      if(computerSelection=="rock"){
+         return ("You Win");
+       }
+       else if(computerSelection=="paper"){
+         return ("Draw");
+       }
+       else if(computerSelection=="scissors"){
+         return ("You Lose");
+       }
+        break;
+    
+      case 'scissors':
+      if(computerSelection=="rock"){
+         return ("You Lose");
+       }
+       else if(computerSelection=="paper"){
+         return ("You Win");
+       }
+       else if(computerSelection=="scissors"){
+         return ("Draw");
+       }
+    
+        break;
+      default:
+        console.log("Unexpected error");
+    }
+}
+
+function game(){
+    let playerScore=0;
+    let computerScore=0;
+    let playerSelection
+    let computerSelection
+    let result
+
+while (playerScore < 5 && computerScore < 5) {
+      playerSelection = prompt("Choose Rock, Paper or Scissors: ").toLowerCase();
+      computerSelection = computerPlay();
+  result = playRound(playerSelection, computerSelection)
+  
+  if(result=="You Win") {
+      playerScore++
+      console.log(result)
+      console.log("Player Score: " + playerScore + "       " + playerSelection );
+      console.log("Computer Score: " + computerScore + "     " + computerSelection);
+      console.log ("======================================================================");
     }
 
+else if   (result=="You Lose") {
+  computerScore++
+  console.log(result)
+  console.log("Player Score: " + playerScore + "       " + playerSelection );
+  console.log("Computer Score: " + computerScore + "     " + computerSelection);
+  console.log ("======================================================================");
 
+}
+else {
+  console.log(result)
+  console.log("Player Score: " + playerScore + "       " + playerSelection );
+  console.log("Computer Score: " + computerScore + "     " + computerSelection);
+  console.log ("======================================================================");
+}
+
+}
+if (playerScore==5) {
+  console.log("Congratulations, You win!")
+}
+else {
+  console.log("Game Over, You lose!")
+}
+}
 game();
