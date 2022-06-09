7ca0b7f6c45100b9952492d2c8cdb4bd640cd62d
yuuki-coder <eidiyuki21@gmail.com>
2022-06-09 16:22:03 -0300

    Finished game() section.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-    function computerPlay(){
+
+   
+   function computerPlay(){
       let randomNumber = Math.floor(Math.random()*3)
       if (randomNumber==0) {
         let result = "rock"
@@ -17,41 +19,49 @@
     }
 
     function playRound(playerSelection, computerSelection) {
+        let scorePlayer=0
+        let scoreComputer=0
      switch (computerSelection) {
-
+      
   case 'rock':
    if(playerSelection=='rock'){
-     return ("Draw! Rock ties with Rock.")
+     return ("Draw")
    }
    else if(playerSelection=='paper'){
-     return ("You win! Paper beats Rock.")
+       ++scorePlayer
+     return ("You Win")
    }
    else if(playerSelection=='scissors'){
-     return ("You lose! Rock beats Scissors.")
+       scoreComputer++
+     return ("You Lose")
    }
     break;
 
   case 'paper':
   if(playerSelection=='rock'){
-     return ("You lose! Paper beats Rock.")
+    scoreComputer++
+     return ("You Lose")
    }
    else if(playerSelection=='paper'){
-     return ("Draw! Paper ties with Paper.")
+     return ("Draw")
    }
    else if(playerSelection=='scissors'){
-     return ("You win! Scissors beats Paper.")
+    scorePlayer++
+     return ("You Win")
    }
     break;
 
   case 'scissors':
   if(playerSelection=='rock'){
-     return ("You win! Rock beats Scissors.")
+    scorePlayer++
+     return ("You Win")
    }
    else if(playerSelection=='paper'){
-     return ("You lose! Scissors beats Paper.")
+    scoreComputer++
+     return ("You Lose")
    }
    else if(playerSelection=='scissors'){
-     return ("Draw! Scissors ties with Scissors.")
+     return ("Draw")
    }
 
     break;
@@ -60,6 +70,35 @@
 }
     }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let scorePlayer=0;
+    let scoreComputer=0;
+   while (scoreComputer < 5 && scorePlayer < 5) {
+
+        let playerSelection = "rock";
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection))
+        let result = playRound(playerSelection, computerSelection)
+        if (result == "You Win"){
+            scorePlayer ++
+        }
+        else if 
+            (result =="You Lose"){
+                scoreComputer++
+            }
+            console.log("Player Score: " + scorePlayer)
+            console.log("Computer Score: " + scoreComputer)
+            console.log ("======================================================================")
+
+        }
+        if(scoreComputer==5) {
+            console.log ("Game over, You lose")
+        }
+        else {
+            console.log("Congratulations, You are the winner!")
+        }
+    
+    }
+
+
+game();
